9f2c640207a62b3e6d5b9d6ab0e73ee3baa72419
cyrilperrin <perrin.cyril.88@gmail.com>
2013-10-13 00:39:50 +0200

    Improve Zend coding standards compliance

--- example/index.php
+++ example/index.php
@@ -4,15 +4,19 @@
 require('../src/terminal_parameters.php');
 
 try {
-	// Get parameters
-	$parameters = terminal_parameters(array(array('s','source'),array('d','dest')),array('h','w'));
-	
-	// Display parameters
-	echo 'Source : "',$parameters[0],'"',"\n",
-	     'Destination : "',$parameters[1],'"',"\n",
-	     'Height : "',$parameters[2],'"',"\n",
-	     'Width : "',$parameters[3],'"',"\n";
+    // Get parameters
+    $parameters = terminal_parameters(
+        array(array('s','source'),
+        array('d','dest')),
+        array('h','w')
+    );
+    
+    // Display parameters
+    echo 'Source : "',$parameters[0],'"',"\n",
+         'Destination : "',$parameters[1],'"',"\n",
+         'Height : "',$parameters[2],'"',"\n",
+         'Width : "',$parameters[3],'"',"\n";
 } catch(Exception $exception) {
-	// Display error
-	echo $exception->getMessage(),"\n";
+    // Display error
+    echo $exception->getMessage(),"\n";
 }
